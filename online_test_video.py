89845f9831bf7c2e2d168361ef3078d4b7759d0c
mathmaxx <math@qq.com>
2022-03-20 02:14:08 +0800

    mute parser.

--- online_test_video.py
+++ online_test_video.py
@@ -600,31 +600,36 @@ class ServerRTC(object):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="WebRTC audio / video / data-channels demo"
-    )
-    parser.add_argument("--cert-file", help="SSL certificate file (for HTTPS)")
-    parser.add_argument("--key-file", help="SSL key file (for HTTPS)")
-    parser.add_argument(
-        "--host", default="0.0.0.0", help="Host for HTTP server (default: 0.0.0.0)"
-    )
-    parser.add_argument(
-        "--port", type=int, default=5000, help="Port for HTTP server (default: 8080)"
-    )
-    parser.add_argument("--record-to", help="Write received media to a file."),
-    parser.add_argument("--verbose", "-v", action="count")
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser(
+    #     description="WebRTC audio / video / data-channels demo"
+    # )
+    # parser.add_argument("--cert-file", help="SSL certificate file (for HTTPS)")
+    # parser.add_argument("--key-file", help="SSL key file (for HTTPS)")
+    # parser.add_argument(
+    #     "--host", default="0.0.0.0", help="Host for HTTP server (default: 0.0.0.0)"
+    # )
+    # parser.add_argument(
+    #     "--port", type=int, default=5000, help="Port for HTTP server (default: 8080)"
+    # )
+    # parser.add_argument("--record-to", help="Write received media to a file."),
+    # parser.add_argument("--verbose", "-v", action="count")
+    # args = parser.parse_args()
+    #
+    # if args.verbose:
+    #     logging.basicConfig(level=logging.DEBUG)
+    # else:
+    #     logging.basicConfig(level=logging.INFO)
+    #
+    # if args.cert_file is None:
+    #     ssl_context = ssl.SSLContext()
+    #     ssl_context.load_cert_chain("example.crt", "example.key")
+    # else:
+    #     ssl_context = None
 
-    if args.verbose:
-        logging.basicConfig(level=logging.DEBUG)
-    else:
-        logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
+    ssl_context = ssl.SSLContext()
+    ssl_context.load_cert_chain("example.crt", "example.key")
 
-    if args.cert_file is None:
-        ssl_context = ssl.SSLContext()
-        ssl_context.load_cert_chain("example.crt", "example.key")
-    else:
-        ssl_context = None
     sv = ServerRTC()
     sv.options = opt
     app = web.Application()
@@ -633,6 +638,5 @@ if __name__ == "__main__":
     app.router.add_get("/client.js", sv.javascript)
     app.router.add_post("/offer", sv.offer)
     web.run_app(
-        app, access_log=None, host=args.host, port=args.port, ssl_context=ssl_context
+        app, access_log=None, host="0.0.0.0", port=5000, ssl_context=ssl_context
     )
-    
